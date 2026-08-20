d117ecc6a9ba79c71666d921bb283f0cfcb5cac7
Jeniffer schurhaus Lucas <jenifferlucas2019@gmail.com>
2026-08-19 21:58:53 -0300

    Add files via upload

---
+++ outros/condicionais.js
@@ -0,0 +1,14 @@
+function exemploscondicionas(){
+    console.log("condicionas.js rodando...");
+   const possuiGraduacao = true;
+const possuiDoutorado = false;
+
+if(possuiGraduacao) {
+  console.log('Possui graduação');
+} else {
+  console.log('Não possui graduação');
+}
+
+}
+
+export default exemploscondicionas;
